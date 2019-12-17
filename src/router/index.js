59ff35b5fcae3082a8f970a271b058efa4fbf087
lujiehui <lujiehui@woouo.com>
2019-12-17 16:40:21 +0800

    '1'

--- src/router/index.js
+++ src/router/index.js
@@ -34,6 +34,17 @@ import Layout from '@/layout'
  */
 export const constantRoutes = [
   {
+    path: '/redirect',
+    component: Layout,
+    hidden: true,
+    children: [
+      {
+        path: '/redirect/:path*',
+        component: () => import('@/views/redirect/index')
+      }
+    ]
+  },
+  {
     path: '/login',
     component: () => import('@/views/login/index'),
     hidden: true
